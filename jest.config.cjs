e4b0db6e9714fb00d3b3d548dd552b64ae220f94
combowomb0 <jonick9009@gmail.com>
2021-05-08 03:00:54 +0500

    add entry point small changes in package.json improve structure

--- jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  moduleNameMapper: {
-    'alt-client': '<rootDir>/__mocks__/alt-client.cjs',
-    'alt-server': '<rootDir>/__mocks__/alt-server.cjs',
-    'natives': '<rootDir>/__mocks__/natives.cjs',
-  }
-}
